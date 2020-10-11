9f2b5aa8959707bc57f6d1203000bf5c1562f0fc
4923 <202003774y@gmail.com>
2020-10-11 21:37:34 +0900

    Update settings.py to prepare hosting

--- heroku/blog_ckeditor/blog_ckeditor/settings.py
+++ heroku/blog_ckeditor/blog_ckeditor/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qhyxuz609@#mfjo0y6c)8g4-nv5vt6s@1$(316g(1#1ul5iz_x'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'qhyxuz609@#mfjo0y6c)8g4-nv5vt6s@1$(316g(1#1ul5iz_x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
